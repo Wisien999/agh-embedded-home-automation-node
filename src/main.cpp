2e6ad85061b5352fbddb3ef898750879e1eaa08f
K3;3 <41787377+Wisien999@users.noreply.github.com>
2023-06-05 21:01:51 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,70 @@
 #include <Arduino.h>
-#include <AsyncMqttClient.h>
-#include <DHT.h>
-#include <ESP8266WiFi.h>
 #include <LiquidCrystal_I2C.h>
-#include <Ticker.h>
+#include <DHT.h>
 #include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
 
 #include "creds.h"
 
-// set the LCD number of columns and rows
-int lcdColumns = 16;
-int lcdRows = 2;
-
-#define DHTTYPE DHT22 // DHT 22
-
-#define MQTT_HOST IPAddress(192, 168, 1, 2)
-#define MQTT_PORT 1883
-
-#define TEMPERATURE_TOPIC "house/hall/temperature"
-#define HUMIDITY_TOPIC "house/hall/humidity"
-
-AsyncMqttClient mqttClient;
-Ticker mqttReconnectTimer;
 
 WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
 Ticker wifiReconnectTimer;
 
-char humidityBuff[5];
-char temperatureBuff[5];
+#define MQTT_HOST IPAddress(192, 168, 1, 2)
+#define MQTT_PORT 1883
+Ticker mqttReconnectTimer;
+AsyncMqttClient mqttClient;
+
+// set the LCD number of columns and rows
+#define lcdColumns 16
+#define lcdRows 2
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+char lcd_message[lcdRows * lcdColumns + 1 + 1];
+
+#define DHT_PIN D4
+Ticker dhtTicker;
+DHT22 dht_sensor;
+volatile float humidity = 0;
+volatile float temperature = 0;
+volatile uint8_t dht_error = 0;
+volatile int8_t dht_result = 0;
+
+Ticker light_ticker;
+volatile uint16_t light_value = 0;
+
+#define PIR_PIN D5
+uint8_t prev_movement = LOW; // LOW is no movement
+
+#define DOOR_PIN D7
+uint8_t prev_door = LOW; // LOW is door closed
+
+
+void read_DHT() {
+  dht_sensor.read();
+}
+
+// this callback will be called from an interrupt
+// it should be short and carry the ICACHE_RAM_ATTR attribute
+void ICACHE_RAM_ATTR handleData(float h, float t) {
+  humidity = h;
+  temperature = t;
+  dht_result = 1;
+}
+
+// this callback will be called from an interrupt
+// it should be short and carry the ICACHE_RAM_ATTR attribute
+void ICACHE_RAM_ATTR handleError(uint8_t e) {
+  Serial.print("DHT error occurred. Error code: ");
+  Serial.println(e);
+  Serial.print("Error: ");
+  Serial.println(dht_sensor.getError());
+  dht_error = e;
+  dht_result = -1;
+}
+
 
 void connectToWifi() {
   Serial.println("Connecting to Wi-Fi...");
@@ -40,33 +76,19 @@ void connectToMqtt() {
   mqttClient.connect();
 }
 
-void onWifiConnect(const WiFiEventStationModeGotIP &event) {
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
   Serial.println("Connected to Wi-Fi.");
   connectToMqtt();
 }
 
-void onWifiDisconnect(const WiFiEventStationModeDisconnected &event) {
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
   Serial.println("Disconnected from Wi-Fi.");
-  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while
-                               // reconnecting to Wi-Fi
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
   wifiReconnectTimer.once(2, connectToWifi);
 }
 
 void onMqttConnect(bool sessionPresent) {
-  Serial.println("Connected to MQTT.");
-  Serial.print("Session present: ");
-  Serial.println(sessionPresent);
-  uint16_t packetIdSub = mqttClient.subscribe("test/lol", 2);
-  Serial.print("Subscribing at QoS 2, packetId: ");
-  Serial.println(packetIdSub);
-  mqttClient.publish("test/lol", 0, true, "test 1");
-  Serial.println("Publishing at QoS 0");
-  uint16_t packetIdPub1 = mqttClient.publish("test/lol", 1, true, "test 2");
-  Serial.print("Publishing at QoS 1, packetId: ");
-  Serial.println(packetIdPub1);
-  uint16_t packetIdPub2 = mqttClient.publish("test/lol", 2, true, "test 3");
-  Serial.print("Publishing at QoS 2, packetId: ");
-  Serial.println(packetIdPub2);
+  uint16_t packetIdPub1 = mqttClient.publish("hall/connected", 1, false, "online");
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
@@ -77,70 +99,40 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
   }
 }
 
-void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
-  Serial.println("Subscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-  Serial.print("  qos: ");
-  Serial.println(qos);
+void read_light() {
+  light_value = analogRead(A0);
+  if (mqttClient.connected()) {
+    char val_str[5];
+    sprintf(val_str, "%d", light_value);
+    mqttClient.publish("hall/light/level", 0, false, val_str);
+  }
 }
 
-void onMqttUnsubscribe(uint16_t packetId) {
-  Serial.println("Unsubscribe acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
 
-void onMqttMessage(char *topic, char *payload,
-                   AsyncMqttClientMessageProperties properties, size_t len,
-                   size_t index, size_t total) {
-  Serial.println("Publish received.");
-  Serial.print("  topic: ");
-  Serial.println(topic);
-  Serial.print("  qos: ");
-  Serial.println(properties.qos);
-  Serial.print("  dup: ");
-  Serial.println(properties.dup);
-  Serial.print("  retain: ");
-  Serial.println(properties.retain);
-  Serial.print("  len: ");
-  Serial.println(len);
-  Serial.print("  index: ");
-  Serial.println(index);
-  Serial.print("  total: ");
-  Serial.println(total);
-}
-
-void onMqttPublish(uint16_t packetId) {
-  Serial.println("Publish acknowledged.");
-  Serial.print("  packetId: ");
-  Serial.println(packetId);
-}
-
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
-// DHT Sensor
-uint8_t DHTPin = D3;
-
-// Initialize DHT sensor.
-DHT dht(DHTPin, DHTTYPE);
-// output control pins
-#define LCD_CONTRAST_PIN 6
-#define LCD_BACKLIGHT_PIN 10
-
-void setup() {
-  lcd.begin(lcdColumns, lcdRows); // initialize LCD
+void setup(){
+  lcd.begin(lcdColumns, lcdRows);// initialize LCD
   lcd.init();
-
-  pinMode(DHTPin, INPUT);
-
-  dht.begin();
-
   lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.clear();
+  lcd.print("Waiting");
+  lcd.setCursor(0, 1);
+  lcd.print("for read");
 
+  pinMode(DHT_PIN, INPUT);
+  pinMode(DOOR_PIN, INPUT_PULLUP);
+  pinMode(PIR_PIN, INPUT);
+  
+  dht_sensor.setPin(DHT_PIN);
+  dht_sensor.onData(handleData);
+  dht_sensor.onError(handleError);
+  dhtTicker.attach(5, read_DHT);
+
+  light_ticker.attach(120, read_light);
+  
   Wire.begin();
   Serial.begin(115200);
 
-  Serial.println();
   Serial.println();
 
   wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
@@ -148,39 +140,38 @@ void setup() {
 
   mqttClient.onConnect(onMqttConnect);
   mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onSubscribe(onMqttSubscribe);
-  mqttClient.onUnsubscribe(onMqttUnsubscribe);
-  mqttClient.onMessage(onMqttMessage);
-  mqttClient.onPublish(onMqttPublish);
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
 
   connectToWifi();
 }
 
-void loop() {
-  float temperature = dht.readTemperature();          // Gets the values of the temperature
-  float humidity = dht.readHumidity();                // Gets the values of the humidity
-  if (isnan(temperature) || isnan(humidity)) {
-    Serial.println("Failed to read from DHT");
-  } else {
-    Serial.print("Temperatura: ");
+
+void loop(){
+  uint8_t current_door_status = digitalRead(DOOR_PIN);
+  if (current_door_status != prev_door) {
+    prev_door = current_door_status;
+    mqttClient.publish("hall/exit-door", 0, false, current_door_status == HIGH ? "opened" : "closed");
+  }
+  uint8_t current_motion = digitalRead(PIR_PIN);
+  if (current_motion != prev_movement) {
+    prev_movement = current_motion;
+    mqttClient.publish("hall/movement", 0, false, current_motion == HIGH ? "motion" : "still");
+  }
+
+  if (dht_result > 0) {
+    Serial.print("Temperature: ");
     Serial.println(temperature);
-    Serial.print("Wilgotność: ");
+    Serial.print("Humidity: ");
     Serial.println(humidity);
 
     lcd.setCursor(0, 0);
     lcd.clear();
-    lcd.print(String("TEMP: ") + String(temperature));
-    lcd.print((char)223); // Degree symbol
+    lcd.print(String("TEMP: ") + String(temperature) );
+    lcd.print((char)223); //Degree symbol
     lcd.print("C");
-    lcd.setCursor(0, 1);
-    lcd.print(String("HUM: ") + String(humidity) + String("%"));
-
-    sprintf(humidityBuff, "%d", (int) humidity);
-    sprintf(temperatureBuff, "%d", (int) temperature);
-    mqttClient.publish(HUMIDITY_TOPIC, 0, false, humidityBuff);
-    mqttClient.publish(TEMPERATURE_TOPIC, 0, false, temperatureBuff);
+    lcd.setCursor(0,1);
+    lcd.print( String("HUM: ") +  String(humidity) + String("%"));
   }
+
   delay(2000);
-  lcd.clear();
 }
